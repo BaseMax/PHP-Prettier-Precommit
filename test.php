4492d16b0104a53210f3eac0a8e871c9b3861feb
Max Base <MaxBaseCode@gmail.com>
2021-07-17 15:24:30 +0430

    run pre-commit and this works well

--- test.php
+++ test.php
@@ -1,11 +1,7 @@
 <?php
-    function sum( $b   , $c)
-    
-    
-      {
-  return $b + $c;     
-      
+function sum($b, $c)
+{
+    return $b + $c;
 }
 
-echo    sum(10     , 10 )
-;
+echo sum(10, 10);
